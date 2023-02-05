a87a259a96722c6c4a98f47ddbc34cce14288ee7
Maximilian Temeschinko <maximilian.temeschinko@contabo.de>
2023-02-05 16:30:50 +0100

    get file event

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+    import fs from 'fs'
+
+	function gotFile(event) {
+        let file = event.target.files[0];
+
+        fs.writeFileSync('static/calli.png', file)
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Hallo</p>
+
+<input type="file" on:input={gotFile} />
