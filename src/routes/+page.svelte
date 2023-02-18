fa859cc0b2410adb0b00201afad863d49652f928
Maximilian Temeschinko <maxiteme@amarket.network>
2023-02-18 12:51:47 +0100

    list static dir + play around with svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,14 @@
 	let fileInput;
 	let files;
 	let avatar;
+	let fileList = ['loading...'];
 
 	function getBase64(image) {
 		const reader = new FileReader();
 		reader.readAsDataURL(image);
 		reader.onload = (e) => {
 			avatar = e.target.result;
-            uploadFunction(e.target.result);
+			uploadFunction(e.target.result);
 		};
 	}
 
@@ -17,6 +18,7 @@
 		const imgData = imgBase64.split(',');
 		data['image'] = imgData[1];
 		console.log(data);
+
 		await fetch(`/upload`, {
 			method: 'POST',
 			headers: {
@@ -26,9 +28,27 @@
 			body: JSON.stringify(data)
 		});
 	}
+
+	async function listFiles() {
+		const lisdir = await fetch('/upload', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+		});
+		const res = await lisdir.json();
+		// res = { ok: 200, dir: [...] }
+		fileList = res.dir;
+		return lisdir;
+	}
+
+  function changeCaption() {
+    document.getElementById('changeMeMofo').innerText = 'Changed';
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<!-- <h1>Welcome to SvelteKit</h1> -->
 <!--<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>-->
 
 <input
@@ -40,6 +60,19 @@
 	bind:this={fileInput}
 	on:change={() => getBase64(files[0])}
 />
-<button class="upload-btn" on:click={() => fileInput.click()}>Upload</button>
 
 <a href="/calli.png">Download</a>
+
+<button on:click={async () => await listFiles()}>List files!</button>
+
+<ol>
+	{#each fileList as filename}
+		<li>{filename}</li>
+	{/each}
+</ol>
+<button on:click={changeCaption}>change it!</button>
+
+<h1 id="changeMeMofo">hallo</h1>
+<h2>hallo</h2>
+<h3>hallo</h3>
+<h4>hallo</h4>
--- src/routes/upload/+server.js
+++ src/routes/upload/+server.js
@@ -3,8 +3,14 @@ import fs from 'fs';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-	const data = await request.json();
-	console.log(data);
-	fs.writeFileSync('static/calli.png', data.image, 'base64');
-	return json({ ok: 200 });
+  const data = await request.json();
+  console.log(data);
+  fs.writeFileSync('static/calli.png', data.image, 'base64');
+  return json({ ok: 200 });
+}
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ request }) {
+  const lsdir = fs.readdirSync('static');
+  return json({ ok: 200, dir: lsdir });
 }
