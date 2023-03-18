c716549693eb251511db5ef67152599d2cb272da
Nicolas Booz <schuechtern.n@gmail.com>
2023-03-18 21:37:29 +0100

    new frontend, missing only progessbar & authentification

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,135 +1,107 @@
+<!-- src/routes/index.svelte -->
 <script>
-	import { onMount } from 'svelte';
-
-	const API_URL = 'http://127.0.0.1:8080/';
-	let fileList;
 	let files = [];
 
-	let file;
-  	let base64String;
+	const uploadServerHost = 'http://84.54.23.140:8080/'
 
-  function handleFileInput(event) {
-    file = event.target.files[0];
-  }
+	async function getFiles() {
+		const response = await fetch(uploadServerHost + 'files');
+		const res = await response.json();
+		files = res.fileList;
+		console.log(files);
+	}
 
-  function convertToBase64() {
-    if (!file) {
-      console.log('Please select a file');
-      return;
-    }
+	async function handleUpload(event) {
+		const selectedFile = event.target.files[0];
+		event.preventDefault();
+		const formData = new FormData();
+		formData.append('uploaded_file', selectedFile);
 
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file);
-    fileReader.onloadend = function() {
-      base64String = fileReader.result.split(',')[1];
-      console.log('Base64 string:', base64String);
-
-	  // TODO: FÜRS NÄCHSTE MAL: SCHICK BASE64 AS HOLE TO C SERVER
-	  // calculate number of chunks and send
-	  // split base64String into chunks and send
-	  uploadFile(base64String);
-
-    };
-  }
-
-
-	// `onMount` gets called once at the very start when page is loaded
-	onMount(async () => {
-		await listFiles();
-	});
-
-	// event handler when form is submitted, i.e. "Upload file" button is clicked
-	// calls API with FormData - FormData includes each value from <input> fields
-	async function uploadFile(base64string) {
-		// const formData = new FormData(event.target);
-		// formData.append('name', files[0].name);
-
-		await fetch(API_URL, {
+		const response = await fetch(uploadServerHost + 'uploads', {
 			method: 'POST',
-			body: base64string
+			body: formData
 		});
-
-		// refresh files after we modified filelist in API
-		await listFiles();
+		await getFiles();
 	}
 
-	// call API to get the current list of files in storage
-	async function listFiles() {
-		const res = await fetch(API_URL, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Accept: 'application/json'
-			}
-		});
-		const resJson = await res.json();
-		fileList = resJson.files;
-	}
-
-	// call API to delete a specific file by filename
-	async function deleteFile(filename) {
-		await fetch(`${API_URL}${filename}`, {
+	async function deleteFromServer(filename) {
+		const response = await fetch(uploadServerHost + `files/${filename}`, {
 			method: 'DELETE'
 		});
-
-		// refresh files after we modified filelist in API
-		await listFiles();
+		await getFiles();
 	}
 </script>
 
-<section class="section">
-	<!-- upload file: -->
-	<form on:submit|preventDefault={uploadFile}>
-		<div class="file has-name">
-			<label class="file-label">
-				<input
-					class="file-input"
-					id="file-to-upload"
-					name="file"
-					type="file"
-					accept="audio/*"
-					bind:files
-				/>
-				<span class="file-cta">
-					<span class="file-icon">
-						<i class="fas fa-upload" />
+<div class="section">
+	<div class="container is-max-desktop">
+		<hr />
+		<h1>File Upload:</h1>
+		<br />
+
+		<div class="form-group column">
+			<div class="file has-name">
+				<label class="file-label">
+					<input on:change={handleUpload} type="file" class="file-input" name="uploaded_file" />
+					<span class="file-cta">
+						<span class="file-icon">
+							<i class="fas fa-upload" />
+						</span>
+						<span class="file-label"> Choose and upload file… </span>
 					</span>
-					<span class="file-label"> Choose a file… </span>
-				</span>
-				<span class="file-name">
-					{#if files.length > 0}
-						{files[0].name}
-					{:else}
-						no file selected ...
-					{/if}
-				</span>
-			</label>
+				</label>
+			</div>
 		</div>
 
-		<button class="button" type="submit"> Upload file </button>
-	</form>
+		<br />
+		<hr />
+		<h2>Uploaded Files:</h2>
+		<br />
 
-	<!-- file list -->
-	<div class="">
-		{#if fileList}
-			<ol>
-				{#each fileList as filename}
-					<figure>
-						<figcaption>{filename}</figcaption>
-						<audio controls src="{API_URL}{filename}" />
-						<button class="button" on:click={() => deleteFile(filename)}>
-							DELETE {filename}
-						</button>
-					</figure>
+		<table class="table is-fullwidth">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Media</th>
+					<th>Download</th>
+					<th>Delete</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each files as file}
+					<tr>
+						<td>{file}</td>
+						<td>
+							<audio controls src= "{uploadServerHost}files/{file}">
+								<a class="forceFontColor" href="{uploadServerHost}files/{file}">Download</a>
+							</audio>
+						</td>
+						<td>
+							<button class="button is-success">
+								<a href="{uploadServerHost}files/{file}" download={file}>Download</a>
+							</button>
+						</td>
+						<td>
+							<button on:click={() => deleteFromServer(file)} class="button is-danger">
+								Delete
+							</button>
+						</td>
+					</tr>
 				{/each}
-			</ol>
-		{:else}
-			<p>No files yet</p>
-		{/if}
+			</tbody>
+		</table>
+
+		{#await getFiles()}
+			<p>Loading...</p>
+		{:then}
+			{#if !files.length}
+				<p>No files have been uploaded yet.</p>
+			{/if}
+		{/await}
 	</div>
+</div>
 
-	
-	<input type="file" on:change="{handleFileInput}">
-	<button on:click="{convertToBase64}">Convert to Base64</button>
-
-</section>
+<style>
+	a {
+		color: white;
+	}
+</style>
