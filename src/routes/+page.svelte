5d015920ae432b421c152a9e388052c4d7023784
Nicolas Booz <schuechtern.n@gmail.com>
2023-03-19 00:32:31 +0100

    second version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
 <!-- src/routes/index.svelte -->
 <script>
-	const uploadServerHost = 'http://84.54.23.140:8080/';
+	// const uploadServerHost = 'http://84.54.23.140:8080/';
+	const uploadServerHost = 'http://localhost:8080/';
 
 	let files = [];
-
-	let progressElement;
-
+	let username;
+	let password;
+	let loginHeader;
 	let UPLOADPROGRESSVALUE = 0;
 
 	async function getFiles() {
 		const response = await fetch(uploadServerHost + 'files');
 		const res = await response.json();
 		files = res.fileList;
-		UPLOADPROGRESSVALUE =0;
+		UPLOADPROGRESSVALUE = 0;
 		console.log(files);
 	}
 
@@ -26,6 +27,7 @@
 		request.upload.addEventListener('progress', handleUploadProgress, false);
 		request.addEventListener('load', async () => await getFiles(), false);
 		request.open('POST', uploadServerHost + 'uploads');
+		request.setRequestHeader('authorization', loginHeader);
 		request.send(formData);
 	}
 
@@ -35,49 +37,99 @@
 
 	async function deleteFromServer(filename) {
 		const response = await fetch(uploadServerHost + `files/${filename}`, {
-			method: 'DELETE'
+			method: 'DELETE',
+			headers: { authorization: loginHeader }
 		});
 		await getFiles();
+	}
+
+	function loginHandler() {
+		loginHeader = 'Basic ' + btoa(username + ':' + password);
 	}
 </script>
 
 <div class="section">
 	<div class="container is-max-desktop">
-		<hr />
-		<h1>File Upload:</h1>
-		<br />
-
-		<div class="form-group column">
-			<div class="file has-name">
-				<label class="file-label">
-					<input on:change={handleUpload} type="file" class="file-input" name="uploaded_file" />
-					<span class="file-cta">
-						<span class="file-icon">
-							<i class="fas fa-upload" />
+		<!-- LOGIN -->
+		<h1 class="subtitle has-text-weight-bold">Login:</h1>
+		<div class="field is-horizontal">
+			<div class="field-body">
+				<div class="field">
+					<p class="control is-expanded has-icons-left">
+						<input bind:value={username} class="input is-success" type="text" placeholder="Name" />
+						<span class="icon is-small is-left">
+							<i class="fas fa-user" />
 						</span>
-						<span class="file-label"> Choose and upload file… </span>
-					</span>
-				</label>
+					</p>
+				</div>
+				<div class="field">
+					<p class="control is-expanded has-icons-left has-icons-right">
+						<input
+							bind:value={password}
+							class="input is-success"
+							type="password"
+							placeholder="Password"
+						/>
+						<span class="icon is-small is-left">
+							<i class="fas fa-envelope" />
+						</span>
+						<span class="icon is-small is-right">
+							<i class="fas fa-check" />
+						</span>
+					</p>
+				</div>
+			</div>
+
+			<div class="field is-horizontal">
+				<div class="field-label">
+					<!-- Left empty for spacing -->
+				</div>
+				<div class="field-body">
+					<div class="field">
+						<div class="control">
+							<button on:click={loginHandler} class="button is-primary"> Login </button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 
-		{#if UPLOADPROGRESSVALUE > 0}
-			<progress class="progress is-info" value={UPLOADPROGRESSVALUE} max="100" />
+		<!-- FILE UPLOAD -->
+		{#if loginHeader}
+			<hr />
+			<h1 class="subtitle has-text-weight-bold">File Upload:</h1>
+			<div class="form-group column">
+				<div class="file has-name">
+					<label class="file-label">
+						<input on:change={handleUpload} type="file" class="file-input" name="uploaded_file" />
+						<span class="file-cta">
+							<span class="file-icon">
+								<i class="fas fa-upload" />
+							</span>
+							<span class="file-label"> Choose and upload file… </span>
+						</span>
+					</label>
+				</div>
+			</div>
+
+			{#if UPLOADPROGRESSVALUE > 0}
+				<progress class="progress is-info" value={UPLOADPROGRESSVALUE} max="100" />
+			{/if}
 		{/if}
 
-
+		<!-- TABLE UPLOADED FILES -->
 		<br />
 		<hr />
-		<h2>Uploaded Files:</h2>
-		<br />
-
+		<h1 class="subtitle has-text-weight-bold">Uploaded Files:</h1>
 		<table class="table is-fullwidth">
 			<thead>
 				<tr>
 					<th>Name</th>
 					<th>Media</th>
-					<th>Download</th>
-					<th>Delete</th>
+					{#if loginHeader}
+						<th>Download</th>
+						<th>Delete</th>
+					{/if}
 				</tr>
 			</thead>
 			<tbody>
@@ -89,16 +141,18 @@
 								<a class="forceFontColor" href="{uploadServerHost}files/{file}">Download</a>
 							</audio>
 						</td>
-						<td>
-							<button class="button is-success">
-								<a href="{uploadServerHost}files/{file}" download={file}>Download</a>
-							</button>
-						</td>
-						<td>
-							<button on:click={() => deleteFromServer(file)} class="button is-danger">
-								Delete
-							</button>
-						</td>
+						{#if loginHeader}
+							<td>
+								<button class="button is-success">
+									<a href="{uploadServerHost}files/{file}" download={file}>Download</a>
+								</button>
+							</td>
+							<td>
+								<button on:click={() => deleteFromServer(file)} class="button is-danger">
+									Delete
+								</button>
+							</td>
+						{/if}
 					</tr>
 				{/each}
 			</tbody>
