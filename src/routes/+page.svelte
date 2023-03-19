7a267ad9185a785ecc9dc078da5604bd70e2af0b
Maximilian Temeschinko <maximilian.temeschinko@contabo.de>
2023-03-19 12:09:32 +0100

    switch to https to call server

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <!-- src/routes/index.svelte -->
 <script>
-	// const uploadServerHost = 'http://84.54.23.140:8080/';
-	const uploadServerHost = 'http://localhost:8080/';
+	const uploadServerHost = 'https://84.54.23.140:8080/';
+	// const uploadServerHost = 'http://localhost:8080/';
 
 	let files = [];
 	let username;
